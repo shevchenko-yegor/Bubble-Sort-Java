e86d97f5f9e50545fd4c953e1a71d0ee2c8837c3
Yehor Shevchenko <shevchenko.egor@gmail.com>
2015-02-04 19:04:07 +0000

    Runner.java
    This file is for example.

---
+++ Runner.java
@@ -0,0 +1,25 @@
+package bubble.sort;
+
+/**
+ *
+ * @author Yehor Shevchenko
+ */
+public class Runner {
+    
+        public static void main(String[] args) {
+            
+            BubbleSort sb = new BubbleSort();
+            int z [] = {1, 32, 3, 4, 5, 6, 7, 10, 9, 8};
+        
+            z = sb.sort(z, "ASC");
+                
+        /*int i = 0;
+        while(i < z.length){
+            System.out.print(z[i] + " ");
+            i++;
+        }
+        System.out.println("");*/
+       
+    }
+    
+}
